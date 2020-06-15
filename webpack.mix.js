b662aea900afd5c1b7a01692e2e47ebd726f376d
AndySong <andylauszp@gmail.com>
2020-06-15 12:21:53 +1200

    first

---
+++ webpack.mix.js
@@ -0,0 +1,9 @@
+const mix = require('laravel-mix');
+
+mix.postCss('resources/css/app.css', 'public/css', [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('postcss-preset-env')({ stage: 1 }),
+  ]);
+
+mix.js('resources/js/app.js', 'public/js');
